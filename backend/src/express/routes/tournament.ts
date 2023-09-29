a2ff10a8a62e3dd1b5c91ee11e1015089f4ca908
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2023-09-29 17:55:09 +0100

    Add "get players" public endpoint

--- backend/src/express/routes/tournament.ts
+++ backend/src/express/routes/tournament.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { ExpressRouter } from '../../types/ExpressRouter';
 import SuperTuxHelper from '../../supertux/SuperTuxHelper';
+import Database from '../../database/Database';
 
 import { AuthMiddleware } from '../middleware/Auth';
 
@@ -18,6 +19,15 @@ export default class TournamentRouter extends ExpressRouter {
         this.router.post('/start', AuthMiddleware.adminTokenNeeded, async (req, res) => {
             SuperTuxHelper.startTournament();
             res.json({ success: true, tournamentStarted: SuperTuxHelper.isTournamentStarted(), tournamentStartedTimestamp: SuperTuxHelper.getTournamentStartedTimestamp() });
+        });
+
+        this.router.get('/players', async (req, res) => {
+            // Remove player id from response
+            const players = Array.from(Database.getPlayers().values()).map(player => {
+                player.id = "000000";
+                return player;
+            });
+            res.json({ success: true, players });
         });
     }
 }
