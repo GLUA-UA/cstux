7f967af9bfd4fba4575024060c8c541c764ba3ea
Miguel Vila <miguelovila@ua.pt>
2024-09-28 18:30:21 +0100

    [NEW] Initialized admin dashboard project

--- admin_dashboard/app/page.tsx
+++ admin_dashboard/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
+              app/page.tsx
             </code>
             .
           </li>
