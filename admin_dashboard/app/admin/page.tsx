42961df7ae9f2c77953d54eafd6484d32cc177c4
Miguel Vila <miguelovila@ua.pt>
2024-10-05 19:51:29 +0100

    add livel list with pagination

--- admin_dashboard/app/admin/page.tsx
+++ admin_dashboard/app/admin/page.tsx
@@ -1,13 +1,26 @@
 import PlayerTable from "@/app/admin/players-table";
+import LevelTable from "@/app/admin/levels-table";
 
 export default function AdminDashboard() {
   return (
     <div className="container m-auto">
-      <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight mt-6 mb-3 text-center">
+      <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight mt-6 mb-6 text-center">
         Administration Dashboard
       </h2>
-
-      <PlayerTable />
+      <div className="flex flex-row gap-6">
+        <div className="basis-1/2">
+          <h3 className="scroll-m-20 text-xl font-semibold tracking-tight mt-6 mb-6 text-center">
+            Player Management
+          </h3>
+          <PlayerTable />
+        </div>
+        <div className="basis-1/2">
+          <h3 className="scroll-m-20 text-xl font-semibold tracking-tight mt-6 mb-6 text-center">
+            Level List
+          </h3>
+          <LevelTable />
+        </div>
+      </div>
     </div>
   );
 }
