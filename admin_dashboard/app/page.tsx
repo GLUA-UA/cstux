3f199c9b687b9d4426adb7b0dcb223d5d55e2df7
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2024-10-21 22:58:11 +0100

    add download from github and homepage
    Signed-off-by: Francisco Ribeiro <francisco.ribeiro@ua.pt>

--- admin_dashboard/app/page.tsx
+++ admin_dashboard/app/page.tsx
@@ -1,12 +1,32 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="container m-auto">
       <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight mt-6 mb-6 text-center">
-        Leaderboard
+        Torneio de SuperTux
       </h2>
-
+      <div className="m-3">
+      <h1 className="font-bold">Download do Cliente</h1>
+        <Button className="mr-3" onClick={() => {
+          window.location.href = "/api/download?platform=windows";
+        }}>
+          Transferir cliente para Windows
+        </Button>
+        <Button className="mx-3" onClick={() => {
+          window.location.href = "/api/download?platform=linux";
+        }}>
+          Transferir cliente para Linux
+        </Button>
+      </div>
+      <div className="m-3">
+        <h1 className="font-bold">Links</h1>
+        <Button onClick={() => { window.location.href = "/leaderboard"; }}>
+          Ir para Leaderboard
+        </Button>
+      </div>
     </div>
   );
 }
