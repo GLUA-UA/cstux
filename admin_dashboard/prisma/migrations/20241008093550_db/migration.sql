e872fd5c069cbcedbb6c3a45829110242dbb0efe
Miguel Vila <miguelovila@ua.pt>
2025-04-03 11:04:03 +0100

    refactor: removed old dashboard and client

--- admin_dashboard/prisma/migrations/20241008093550_db/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[statName]` on the table `states` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "states_statName_key" ON "states"("statName");
